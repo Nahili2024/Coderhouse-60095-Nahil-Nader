d3e9b5ccdbe27ac684d08ad916b665dbf7787eea
Nader <Nahili.Nader@molinos.com.ar>
2024-12-11 22:05:07 -0300

    Creacion de modelos

--- src/minegocio/admin.py
+++ src/minegocio/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Vendedor, Producto, Transaccion, Reporte   
+
+@admin.register(Vendedor)
+class VendedorAdmin(admin.ModelAdmin):
+    list_display = ("nombre","dni")
+
+@admin.register(Transaccion)
+class TransaccionAdmin(admin.ModelAdmin):
+    list_display = ("dni","fecha","cantidad","producto","monto")
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("categoria")
+
+@admin.register(Reporte)
+class ReporteAdmin(admin.ModelAdmin):
+    list_display = ("dni","año","informe","periodo")
--- src/minegocio/models.py
+++ src/minegocio/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=13)
+    dni = models.PositiveIntegerField(max_length=8, unique=True)
+
+    def _str_(self):
+        return f"{self.nombre}, {self.dni}"
+    
+class Producto(models.Model):  
+    categorias = [("C","Celulares"),
+                 ("AU","Auriculares"),
+                 ("PE","Pequeños electrodomesticos"),
+                 ("T","Tablets")]
+    categoria = models.CharField(max_length=50,choices=categorias,default="PE")
+    
+
+class Transaccion(models.Model):
+    dni = models.ForeignKey(Vendedor,on_delete=models.SET_NULL)
+    fecha = models.DateField(null=True,blank=True)
+    cantidad = models.PositiveIntegerField(max_length=5)
+    monto = models.DecimalField(max_digits=10,decimal_places=2)
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
+
+
+class Reporte(models.Model):
+    dni = models.ForeignKey(Vendedor,on_delete=models.SET_NULL,null=True)
+    año = models.PositiveIntegerField()
+    informe = models.CharField(max_length=25)
+    categorias = models.ForeignKey(Producto, on_delete=models.CASCADE,null=True)
+    periodos = [
+        ('MENSUAL', 'Mensual'),
+        ('TRIMESTRAL', 'Trimestral'),
+        ('ANUAL', 'Anual'),
+    ]
+    periodo = models.CharField(max_length=10,choices=periodos,default='ANUAL')
+
+
+    class Meta():
+        constraints = [models.UniqueConstraint(fields=["año","categorias","periodo"], name="unico_informe_por_año")]
